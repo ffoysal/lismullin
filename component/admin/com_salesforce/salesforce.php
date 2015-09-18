ca45e223fc9db6365b991f5255dfd77a16db8f08
Foysal <foysal.iqbal.fb@gmail.com>
2015-09-18 11:05:30 -0400

    Making joomla component structure

--- component/admin/com_salesforce/salesforce.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('_JEXEC') or die('Access deny');
-
-jimport('joomla.application.component.controller');
-
-$controller = JController::getInstance('Salesforce');
-$controller->execute(JRequest::getCmd('task'));
-$controller->redirect();
--- component/admin/com_salesforce/uninstall.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE `#__salesforce_invoice_number`;
-DROP TABLE `#__salesforce_pdftemplate`;
-DROP TABLE `#__salesforce_emailtemplate` 
